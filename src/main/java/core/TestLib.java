d07373abaa9e2953819f4fd0a905954083e9b746
reachasquared <37804823+reachasquared@users.noreply.github.com>
2018-04-30 12:38:01 -0400

    Update TestLib.java

--- src/main/java/core/TestLib.java
+++ src/main/java/core/TestLib.java
@@ -15,77 +15,7 @@ import static org.junit.Assert.fail;
  * Created by snamburi on 4/30/2017.
  */
 public abstract class TestLib {
-    public WebDriver driver;
-    public String baseUrl;
-    public boolean acceptNextAlert = true;
-    public StringBuffer verificationErrors = new StringBuffer();
-    public Logger logger = LogManager.getLogger("test");
-
-    @Before
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = "http://newtours.demoaut.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    public boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
-
-    /**
-     * Locating Input field by label
-     */
-    public static String getInputElementByLabel(String expectedLabel) {
-        String inputLocator = "//b[contains(text(),'%s')]/ancestor::td[1]/following-sibling::td/input";
-        return String.format(inputLocator, expectedLabel);
-    }
-
-    /**
-     * Locating Input field by label
-     */
-    public static String getInputElementByName(String elementName) {
-        String inputLocator = "//input[@name='%s']))";
-        return String.format(inputLocator, elementName);
-    }
-
+    
     public void whoAmI(){
         System.out.println("baseClass : I am in Super or Base Class");
     }
