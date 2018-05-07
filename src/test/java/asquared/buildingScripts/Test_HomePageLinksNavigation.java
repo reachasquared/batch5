3b9b579e43b3ef7331df070f9267847d3dc381b6
reachasquared <37804823+reachasquared@users.noreply.github.com>
2018-05-07 10:25:44 -0400

    Add files via upload

--- src/test/java/asquared/buildingScripts/Test_HomePageLinksNavigation.java
+++ src/test/java/asquared/buildingScripts/Test_HomePageLinksNavigation.java
@@ -1,14 +1,36 @@
 package asquared.buildingScripts;
 
-public class Test_HomePageLinksNavigation {
+import core.baseClass;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Test_HomePageLinksNavigation extends baseClass {
+    @Test
     public void homePageLinksNavigation(){
+
+
         //Go to http://newtours.demoaut.com
+        logger.info("Opening web page "+ baseUrl);
+        driver.get(baseUrl);
 
         // Verify page title : Welcome: Mercury Tours
+        logger.info("Verify page title : Welcome: Mercury Tours");
 
         //Click on SIGN-ON link in Header
+        logger.info("Verifying SIGN-ON link exists");
+        assertTrue(driver.findElement(By.linkText("SIGN-ON")).isDisplayed());
+
+        logger.info("Clickingon the link SIGN-ON");
+        driver.findElement(By.linkText("SIGN-ON")).click();
 
         // Verify page title : Sign-on: Mercury Tours
+        logger.info("Verify page title : Sign-on: Mercury Tours");
+        assertEquals("Sign-on: Mercury Tours", driver.getTitle());
 
         //Click on SUPPORT link in Header
 
--- src/test/java/asquared/buildingScripts/Test_HomePageLinksVerification.java
+++ src/test/java/asquared/buildingScripts/Test_HomePageLinksVerification.java
@@ -1,18 +1,34 @@
 package asquared.buildingScripts;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Test_HomePageLinksVerification {
+    @Test
     public void homePageLinksVerification(){
-        // Go to http://newtours.demoaut.com/
+        //Class     object  = new class name()
+        WebDriver driver = new FirefoxDriver();
+        String baseURL = "http://newtours.demoaut.com/";
 
-        // Verify webpage loads
+        // Go to http://newtours.demoaut.com/
+        driver.get(baseURL);
+
+        // Verify webpage loads "Welcome: Mercury Tours"
+        assertEquals("Welcome: Mercury Tours", driver.getTitle());
 
         // Verify Home link exist
+        assertEquals("Home", driver.findElement(By.linkText("Home")).getText());
 
         // Verify Flights link exist
+        assertTrue(driver.findElement(By.linkText("Flights")).isDisplayed());
 
         // Verify Hotels link exist
+        assertTrue(driver.findElement(By.linkText("Hotels")).isDisplayed());
 
         //Verify Cars link exist
 
