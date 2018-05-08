ff29ab6f4425148f65dcbaa9a18434ae4c34f649
reachasquared <37804823+reachasquared@users.noreply.github.com>
2018-05-08 09:09:36 -0400

    Add files via upload

--- src/test/java/asquared/buildingScripts/Test_newToursRegistration.java
+++ src/test/java/asquared/buildingScripts/Test_newToursRegistration.java
@@ -1,16 +1,39 @@
 package asquared.buildingScripts;
 
-public class Test_newToursRegistration {
-    public void newToursRegistration(){
+import core.baseClass;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Test_newToursRegistration extends baseClass {
+    public void newToursRegistration() throws InterruptedException {
         // Go to http://newtours.demoaut.com/
+        logger.info("Opening webpage : " + baseUrl);
+        driver.get(baseUrl);
 
         // Verify webpage loads
+        logger.info("Verify webpage loads");
+        assertEquals("Welcome: Mercury Tours", driver.getTitle());
 
         // Click on REGISTER link in Header
+        logger.info("Click on REGISTER link in Header");
+        assertTrue(driver.findElement(By.linkText("REGISTER")).isDisplayed());
+        driver.findElement(By.linkText("REGISTER")).click();
+
+        //Verify page title: Register: Mercury Tours
+        assertEquals("Register: Mercury Tours", driver.getTitle());
 
         // Enter the data into fields
+        logger.info("entering data");
         //First name : Reach
+        logger.info("First name : Reach");
+        driver.findElement(By.name("firstName")).sendKeys("Reach");
+
         //Last name : Asquared
+        isElementPresent(By.name("lastName"));
+        driver.findElement(By.name("lastName")).sendKeys("Asquared");
+
         //Email : testing.automation@gmail.com
         //Address: 123 Baymeadows way
         //City : Jacksonville
