a69feaabcba1bb33dafe332c866da9b9a8a5bd31
reachasquared <37804823+reachasquared@users.noreply.github.com>
2018-05-09 11:38:33 -0400

    Add files via upload

--- src/test/java/asquared/buildingScripts/Test_newToursRegistrationXpath.java
+++ src/test/java/asquared/buildingScripts/Test_newToursRegistrationXpath.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class Test_newToursRegistration extends baseClass {
+public class Test_newToursRegistrationXpath extends baseClass {
     public void newToursRegistration() {
         // Go to http://newtours.demoaut.com/
         logger.info("Opening webpage : " + baseUrl);
@@ -14,46 +14,34 @@ public class Test_newToursRegistration extends baseClass {
 
         // Verify webpage loads
         logger.info("Verify webpage loads");
+        //using selenium method
         assertEquals("Welcome: Mercury Tours", driver.getTitle());
+        //using xpath
+        assertEquals("Welcome: Mercury Tours", driver.findElement(By.xpath("//title")).getText());
 
         // Click on REGISTER link in Header
         logger.info("Click on REGISTER link in Header");
-/*
-        assertTrue(driver
-                .findElement(By.linkText("REGISTER"))
-                .isDisplayed());
-*/
-
-        assertTrue(
-                driver
-                .findElement(
-                        By.xpath("//a[@href='mercuryregister.php']")
-                ).isDisplayed()
-        );
-        //driver.findElement(By.linkText("REGISTER")).click();
-        driver.findElement(By.xpath("//a[@href='mercuryregister.php']"))
-                .click();
+        //verify link exist
+        assertTrue(driver.findElement(By.xpath("//a[@href='mercuryregister.php']")).isDisplayed());
+        //click on Register link
+        driver.findElement(By.xpath("//a[@href='mercuryregister.php']")).click();
 
         //Verify page title: Register: Mercury Tours
-        assertEquals("Register: Mercury Tours", driver.getTitle());
+        assertEquals("Register: Mercury Tours", driver.findElement(By.xpath("//title")).getText());
 
         // Enter the data into fields
         logger.info("entering data");
         //First name : Reach
         logger.info("First name : Reach");
-        //driver.findElement(By.name("firstName")).sendKeys("Reach");
-        driver.findElement(
-                By.xpath("//input[@name='firstName']")
-        ).clear();
-
-        driver.findElement(
-                By.xpath("//input[@name='firstName']")
-        ).sendKeys("Reach");
+        //Clear input field
+        driver.findElement(By.xpath("//input[@name='firstName']")).clear();
+        //Enter data
+        driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Reach");
 
         //Last name : Asquared
-        isElementPresent(By.name("lastName"));//Verify element present
-        driver.findElement(By.name("lastName"))
-                .sendKeys("Asquared");
+        isElementPresent(By.xpath("//input[@name='firstName']"));//Verify element present
+        driver.findElement(By.xpath("//input[@name='firstName']")).clear();//Clear input field
+        driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Asquared");//Enter data
 
         //Email : testing.automation@gmail.com
         //Address: 123 Baymeadows way
